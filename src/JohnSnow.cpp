11a0b4c4acf721b8eec3a2e40a909afa87b62a65
t0mo0n <150349492+t0mo0n@users.noreply.github.com>
2024-10-16 23:11:45 +0800

    Ken的第二次Pr (#12)
    * 完成部分类的内容(注意main.cpp)已经被我改动,还没有改回来
    
    * 基本完成第一个防御塔和投掷物的基类
    
    * 小存
    
    * 更新防御塔类的文档和防御塔命名规范,添加防御塔自动将删除的炮弹移除数组的操作
    
    * 删除提交的main.cpp
    
    * 修改文档
    
    * nothing
    
    * mmm
    
    * 不带main.cpp和MovingItem
    
    * 修复部分关于projectile和Tower的bug
    
    * 小存
    
    * 增加tower消亡的发射信号,和增加自动settarget
    
    * 修改发射子弹bug,修改检测范围函数使之攻击最近的敌人,添加槽函数向敌人发送关于炮弹类型和伤害的信号
    
    * 保存FindEnemy函数的版本
    
    * 完成投石车类,修改防御塔基类
    
    * 完成投石车的debug
    
    * merge前保存
    
    * ..
    
    * ...
    
    * Add .gitignore
    
    * Add .gitignore
    
    * Add .gitignore new
    
    * 基本完成JohnSnow类
    
    * renew .gitignore
    
    * 完成除龙炎弹不显示的dragon类
    
    ---------
    
    Co-authored-by: Ken <1521993813@qq.com>

--- src/JohnSnow.cpp
+++ src/JohnSnow.cpp
@@ -1,4 +1,130 @@
 #include "JohnSnow.h"
+// 还没有添加友军增益效果
+JohnSnow::JohnSnow(QPoint pos_)
+    : TowerFrame(pos_, 3)
+{
+    projectType = 10; // 没有投掷物
+    attackRange = 100;
+    attackSpeed = 800;
+    attackTime = 0;
+    buyCost = 1000;
+    sellPrice = 900;
+    picDir = ":/img/asset/1.png";
+    towerType = 3;
+    hurt = 400;
+    upgradeFee.push_back(1000);
+    upgradeFee.push_back(2000);
+    upgradeFee.push_back(3000);
 
-JohnSnow::JohnSnow()
-{}
+    attackTimer->start(attackSpeed);
+}
+
+void JohnSnow::FindEnemy()
+{
+    if (target != nullptr)
+    {
+        // 获取塔的位置
+        QPointF towerPos = this->pos();
+        // 获取目标的位置
+        QPointF targetPos = target->pos();
+
+        // 计算方向
+        qreal angle = std::atan2(targetPos.y() - towerPos.y(), targetPos.x() - towerPos.x());
+
+        // 设置塔的旋转（如果需要旋转显示）
+        setRotation(angle * 180.0 / M_PI); // 将弧度转换为度
+    }
+    if (target == nullptr)
+    {
+        // qDebug()<<"tower攻击的目标无效";
+        QList<QGraphicsItem *> itemsInBoundingRect = checkForItemsInBoundingRect();
+
+        if (!itemsInBoundingRect.isEmpty())
+        {
+            Enemy *min_item = nullptr;
+            qreal min_distance = attackRange;
+            for (auto *item : itemsInBoundingRect)
+            {
+                Enemy *enemy_p = dynamic_cast<Enemy *>(item);
+                if (enemy_p == nullptr)
+                {
+                    continue;
+                }
+                qreal distance = QLineF(QPointF(enemy_p->pos().x() + enemy_p->size / 2, enemy_p->pos().y() + enemy_p->size / 2), this->TowerCentral).length() - 10;
+                if (distance <= min_distance)
+                {
+                    min_item = enemy_p;
+                }
+            }
+            if (min_item)
+            {
+                // qDebug()<<"设置新目标";
+                setTarget(min_item);
+                connect(this, &JohnSnow::snowAttack, target, &Enemy::recieveSnow);
+            }
+        }
+        else
+        {
+            resetTarget();
+        }
+    }
+}
+
+void JohnSnow::attack()
+{
+    if (target)
+    {
+        if (this->collidesWithItem(target))
+        {
+
+            // 获取塔的位置
+            QPointF towerPos = this->pos();
+            // 获取目标的位置
+            qreal bulletX = towerPos.x() + towerSize / 2;
+            qreal bulletY = towerPos.y() + towerSize / 2;
+            QPointF bulletStartPos(bulletX, bulletY);
+            // 创建并发射投掷物
+            Wolf *bullet = nullptr;
+
+            assert(1 <= level && level <= 2);
+
+            emit snowAttack(hurt);
+            attackTime++;
+
+            if (level == 2 && attackTime % 4 == 0)
+            {
+                bullet = new Wolf(bulletStartPos, TowerCentral, attackRange);
+                if (bullet)
+                {
+                    projectileList.push_back(bullet);
+
+                    connect(bullet, &Projectile::outrange, this, [this, bullet]()
+                            {
+                        projectileList.removeOne(bullet);  // 从列表中移除该子弹
+                        scene()->removeItem(bullet);
+                        delete bullet; });
+                    bullet->setTarget(target);
+                    scene()->addItem(bullet);
+                }
+            }
+        }
+        else
+        {
+
+            resetTarget();
+        }
+    }
+}
+
+void JohnSnow::upgrade()
+{
+    if (level >= 2)
+    {
+        return;
+    }
+    level++;
+    attackRange += 100;
+    attackSpeed += 100;
+    projectType = 3;
+    update();
+}
